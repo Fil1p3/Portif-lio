ffbb0d0fd5a66b6e11e341cb4a6e0ae906c88984
Fil1p3 <164559658+Fil1p3@users.noreply.github.com>
2024-06-22 23:49:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-// Função para adicionar comentário
 function addComment() {
     const commentInput = document.getElementById('commentInput');
     const commentSection = document.getElementById('commentSection');
     const commentText = commentInput.value.trim();
     
-    // Verifica se o campo de comentário não está vazio
     if (commentText) {
-        // Cria um elemento de div para o comentário
+        
         const commentDiv = document.createElement('div');
         commentDiv.className = 'comment';
         commentDiv.textContent = commentText;
-        
-        // Adiciona o comentário à seção de comentários
         commentSection.appendChild(commentDiv);
-        
-        // Limpa o campo de entrada de comentário
         commentInput.value = '';
     }
 }
